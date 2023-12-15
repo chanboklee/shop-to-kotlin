2735d0a4df025ee490d3a8396a33d7dd933b1528
chanboklee <chanboklee@naver.com>
2023-12-15 21:16:29 +0900

    feat: findAllByProductSellingStatusIn 통해 판매중, 판매보류인 상품리스트를 가져온다.

--- src/main/kotlin/com/lee/shop/domain/product/ProductRepository.kt
+++ src/main/kotlin/com/lee/shop/domain/product/ProductRepository.kt
@@ -3,4 +3,5 @@ package com.lee.shop.domain.product
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface ProductRepository: JpaRepository<Product, Long> {
+    fun findAllByProductSellingStatusIn(productSellingStatusList: List<ProductSellingStatus>): List<Product>
 }
