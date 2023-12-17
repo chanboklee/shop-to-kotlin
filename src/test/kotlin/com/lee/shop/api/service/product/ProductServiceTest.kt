090e86def08a3d018fb1fe015dea54e595edc567
chanboklee <chanboklee@naver.com>
2023-12-17 15:57:55 +0900

    test: 신규 상품 등록 시 상품번호는 가장 최근의 상품번호에서 1 증가한 값인 테스트 코드 작성

--- src/test/kotlin/com/lee/shop/api/service/product/ProductServiceTest.kt
+++ src/test/kotlin/com/lee/shop/api/service/product/ProductServiceTest.kt
@@ -1,8 +1,10 @@
 package com.lee.shop.api.service.product
 
 import com.lee.shop.api.service.product.request.ProductSaveServiceRequest
+import com.lee.shop.domain.product.Product
 import com.lee.shop.domain.product.ProductRepository
 import com.lee.shop.domain.product.ProductSellingStatus.SELLING
+import com.lee.shop.domain.product.ProductType.BOTTOM
 import com.lee.shop.domain.product.ProductType.TOP
 import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.AfterEach
@@ -35,5 +37,21 @@ class ProductServiceTest @Autowired constructor(
         assertThat(productResponse.id).isNotNull
         assertThat(productResponse).extracting("productNumber", "name", "price", "stockQuantity", "productType", "productSellingStatus")
             .containsExactly("001", "맨투맨", 12000, 20, TOP, SELLING)
+    }
+
+    @DisplayName("신규 상품을 등록하면 상품번호는 가장 최근에 등록한 상품의 상품번호에서 1 증가한 갑이다.")
+    @Test
+    fun saveProduct(){
+        // given
+        val product = Product.fixture("001", "청바지", 25000, 30, BOTTOM, SELLING)
+        productRepository.save(product)
+
+        // when
+        val saveProduct = ProductSaveServiceRequest.fixture("맨투맨", 12000, 20, TOP, SELLING)
+        val productResponse = productService.saveProduct(saveProduct)
+
+        // then
+        assertThat(productResponse).extracting("productNumber", "name", "price", "stockQuantity", "productType", "productSellingStatus")
+            .containsExactly("002", "맨투맨", 12000, 20, TOP, SELLING)
     }
 }
