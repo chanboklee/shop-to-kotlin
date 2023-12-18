5e4b852bba62b75f4ac086c69453ffb1a99ecc3b
chanboklee <chanboklee@naver.com>
2023-12-19 00:12:33 +0900

    test: 가입된 회원의 전체 리스트를 조회 테스트 코드 작성 (MemberService - getMembers)

--- src/test/kotlin/com/lee/shop/api/service/member/MemberServiceTest.kt
+++ src/test/kotlin/com/lee/shop/api/service/member/MemberServiceTest.kt
@@ -1,8 +1,10 @@
 package com.lee.shop.api.service.member
 
 import com.lee.shop.api.service.member.request.MemberSaveServiceRequest
+import com.lee.shop.domain.member.Member
 import com.lee.shop.domain.member.MemberRepository
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.groups.Tuple.tuple
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -32,5 +34,27 @@ class MemberServiceTest @Autowired constructor(
         // then
         assertThat(memberResponse).extracting("name", "email", "password")
             .containsExactly("이찬복", "chanboklee@naver.com", "1234")
+    }
+
+    @DisplayName("가입된 회원의 전체 리스트를 조회한다.")
+    @Test
+    fun getMembers(){
+        // given
+        val member1 = Member.fixture(null, "이찬복", "chanboklee@naver.com", "1234")
+        val member2 = Member.fixture(null, "홍길동", "hong@naver.com", "1234")
+        val member3 = Member.fixture(null, "춘향이", "chun@naver.com", "1234")
+        memberRepository.saveAll(listOf(member1, member2, member3))
+
+        // when
+        val memberResponseList = memberService.getMembers()
+
+        // then
+        assertThat(memberResponseList).hasSize(3)
+        assertThat(memberResponseList).extracting("name", "email", "password")
+            .containsExactlyInAnyOrder(
+                tuple("이찬복", "chanboklee@naver.com", "1234"),
+                tuple("홍길동", "hong@naver.com", "1234"),
+                tuple("춘향이", "chun@naver.com", "1234"),
+            )
     }
 }
