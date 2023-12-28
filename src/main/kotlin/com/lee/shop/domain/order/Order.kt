11100900d6185d6239183031298320246a83dc4d
chanboklee <chanboklee@naver.com>
2023-12-28 20:01:33 +0900

    feat: 주문생성 및 취소 로직 추가

--- src/main/kotlin/com/lee/shop/domain/order/Order.kt
+++ src/main/kotlin/com/lee/shop/domain/order/Order.kt
@@ -1,5 +1,8 @@
 package com.lee.shop.domain.order
 
+import com.lee.shop.domain.delivery.Delivery
+import com.lee.shop.domain.delivery.DeliveryStatus
+import com.lee.shop.domain.orderproduct.OrderProduct
 import jakarta.persistence.*
 
 @Table(name = "orders")
@@ -11,9 +14,33 @@ class Order(
     val id: Long? = null,
 
     @Enumerated(EnumType.STRING)
-    val orderStatus: OrderStatus,
+    var orderStatus: OrderStatus,
 
     @OneToMany(mappedBy = "order")
-    val orders: MutableList<Order> = mutableListOf(),
+    val orderProducts: MutableList<OrderProduct> = mutableListOf(),
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
+    @JoinColumn(name = "delivery_id")
+    val delivery: Delivery,
 ) {
+
+    fun cancel(){
+        if(delivery.deliveryStatus == DeliveryStatus.COMP){
+            throw IllegalArgumentException("배송완료된 상품은 취소가 불가능합니다.")
+        }
+        orderStatus = OrderStatus.CANCEL
+    }
+
+    companion object {
+        fun createOrder(
+            orderProducts: MutableList<OrderProduct>,
+            delivery: Delivery,
+        ): Order{
+            return Order(
+                orderStatus = OrderStatus.ORDER,
+                orderProducts = orderProducts,
+                delivery = delivery,
+            )
+        }
+    }
 }
