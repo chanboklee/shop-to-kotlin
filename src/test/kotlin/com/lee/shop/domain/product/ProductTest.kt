3030a0c11bde5a242349d92467dc914da5c618bb
chanboklee <chanboklee@naver.com>
2024-01-07 19:46:30 +0900

    test : Product addStock 테스트 작성

---
+++ src/test/kotlin/com/lee/shop/domain/product/ProductTest.kt
@@ -0,0 +1,36 @@
+package com.lee.shop.domain.product
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.transaction.annotation.Transactional
+
+@Transactional
+@SpringBootTest
+class ProductTest @Autowired constructor(
+    private val productRepository: ProductRepository,
+){
+
+    @AfterEach
+    fun tearDown() {
+        productRepository.deleteAllInBatch()
+    }
+
+    @DisplayName("주문을 취소하면 상품의 재고가 증가한다.")
+    @Test
+    fun addStock(){
+        // given
+        val product = Product.fixture("001", "맨투맨", 12000, 20, ProductType.TOP, ProductSellingStatus.SELLING)
+        val savedProduct = productRepository.save(product)
+
+        // when // then
+        savedProduct?.addStock(10)
+        val findProduct = productRepository.findByIdOrNull(savedProduct.id!!)
+        Assertions.assertThat(findProduct?.stockQuantity).isEqualTo(30)
+    }
+}
