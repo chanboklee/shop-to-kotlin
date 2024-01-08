d2a4852bac8c20e99ea29c9bd99e2c52c874d656
chanboklee <chanboklee@naver.com>
2024-01-08 21:16:05 +0900

    test : Product removeStock 테스트 작성

--- src/test/kotlin/com/lee/shop/domain/product/ProductTest.kt
+++ src/test/kotlin/com/lee/shop/domain/product/ProductTest.kt
@@ -1,6 +1,6 @@
 package com.lee.shop.domain.product
 
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.DisplayName
@@ -31,6 +31,19 @@ class ProductTest @Autowired constructor(
         // when // then
         savedProduct?.addStock(10)
         val findProduct = productRepository.findByIdOrNull(savedProduct.id!!)
-        Assertions.assertThat(findProduct?.stockQuantity).isEqualTo(30)
+        assertThat(findProduct?.stockQuantity).isEqualTo(30)
+    }
+
+    @DisplayName("주문을 하면 주문수만큼 재고가 차감된다.")
+    @Test
+    fun removeStock(){
+        // given
+        val product = Product.fixture("001", "맨투맨", 12000, 20, ProductType.TOP, ProductSellingStatus.SELLING)
+        val savedProduct = productRepository.save(product)
+
+        // when // then
+        savedProduct?.removeStock(10)
+        val findProduct = productRepository.findByIdOrNull(savedProduct.id!!)
+        assertThat(findProduct?.stockQuantity).isEqualTo(10)
     }
 }
