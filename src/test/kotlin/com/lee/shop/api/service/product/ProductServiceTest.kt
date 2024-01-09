367861c86cae945f39e058f45ae611fed881e91f
chanboklee <chanboklee@naver.com>
2024-01-09 21:24:06 +0900

    test : ProductService getSellingProducts 테스트 작성

--- src/test/kotlin/com/lee/shop/api/service/product/ProductServiceTest.kt
+++ src/test/kotlin/com/lee/shop/api/service/product/ProductServiceTest.kt
@@ -3,9 +3,10 @@ package com.lee.shop.api.service.product
 import com.lee.shop.api.service.product.request.ProductSaveServiceRequest
 import com.lee.shop.domain.product.Product
 import com.lee.shop.domain.product.ProductRepository
-import com.lee.shop.domain.product.ProductSellingStatus.SELLING
-import com.lee.shop.domain.product.ProductType.BOTTOM
-import com.lee.shop.domain.product.ProductType.TOP
+import com.lee.shop.domain.product.ProductSellingStatus
+import com.lee.shop.domain.product.ProductSellingStatus.*
+import com.lee.shop.domain.product.ProductSellingStatus.Companion.forDisplay
+import com.lee.shop.domain.product.ProductType.*
 import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.*
@@ -53,5 +54,29 @@ class ProductServiceTest @Autowired constructor(
         // then
         assertThat(productResponse).extracting("productNumber", "name", "price", "stockQuantity", "productType", "productSellingStatus")
             .containsExactly("002", "맨투맨", 12000, 20, TOP, SELLING)
+    }
+
+    @DisplayName("상품상태가 판매중, 판매보류인 상품 리스트만 조회한다.")
+    @Test
+    fun getSellingProducts(){
+        // given
+        val product1 = Product.fixture("001", "맨투맨", 25000, 100, TOP, SELLING)
+        val product2 = Product.fixture("002", "귀걸이", 40000, 20, ACCESSORIES, STOP_SELLING)
+        val product3 = Product.fixture("003", "패딩", 120000, 82, ACCESSORIES, HOLD)
+        val product4 = Product.fixture("004", "청바지", 12000, 50, BOTTOM, SELLING)
+
+        productRepository.saveAll(listOf(product1, product2, product3, product4))
+
+        // when
+        val products = productRepository.findAllByProductSellingStatusIn(forDisplay())
+
+        // then
+        assertThat(products).hasSize(3)
+            .extracting("productNumber", "name", "price", "stockQuantity", "productType", "productSellingStatus")
+            .containsExactlyInAnyOrder(
+                tuple("001", "맨투맨", 25000, 100, TOP, SELLING),
+                tuple("003", "패딩", 120000, 82, ACCESSORIES, HOLD),
+                tuple("004", "청바지", 12000, 50, BOTTOM, SELLING),
+            )
     }
 }
